6f6c22d328d6ce11d5158605099a4743101c797c
Pooja-Varshney <pooja.var4@gmail.com>
2022-06-22 19:55:30 +0530

    Changes in service 2 for common DB between service 1 and 2

--- PensionPortalService2/src/main/java/com/tcs/PensionPortalApplication.java
+++ PensionPortalService2/src/main/java/com/tcs/PensionPortalApplication.java
@@ -1,13 +1,24 @@
 package com.tcs;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class PensionPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PensionPortalApplication.class, args);
+	}
+	
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseaServer() throws SQLException {
+	    return Server.createTcpServer(
+	      "-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
 	}
 
 }
